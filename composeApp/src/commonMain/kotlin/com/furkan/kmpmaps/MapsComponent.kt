0b6655d7a83476c61fe68e7a5217176cb11b3b28
321Proteus <bgproteus@gmail.com>
2025-02-27 15:18:07 +0100

    Fixed MapsComponent display on iOS

--- composeApp/src/commonMain/kotlin/com/furkan/kmpmaps/MapsComponent.kt
+++ composeApp/src/commonMain/kotlin/com/furkan/kmpmaps/MapsComponent.kt
@@ -3,6 +3,4 @@ package com.furkan.kmpmaps
 import androidx.compose.runtime.Composable
 
 @Composable
-expect fun MapsComponent() {
-
-}
+expect fun MapsComponent()
--- composeApp/src/nativeMain/kotlin/com/furkan/kmpmaps/MapsComponent.native.kt
+++ composeApp/src/nativeMain/kotlin/com/furkan/kmpmaps/MapsComponent.native.kt
@@ -1,7 +1,13 @@
 package com.furkan.kmpmaps
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.interop.UIKitView
+import androidx.compose.ui.unit.dp
 import cocoapods.Google_Maps_iOS_Utils.GMSCameraPosition
 import cocoapods.Google_Maps_iOS_Utils.GMSCameraUpdate
 import cocoapods.Google_Maps_iOS_Utils.GMSMapView
@@ -19,4 +25,11 @@ actual fun MapsComponent() {
     )
     val cameraUpdate = GMSCameraUpdate.setCamera(cameraPosition)
     mapView.moveCamera(cameraUpdate)
+
+    UIKitView(
+        modifier = Modifier
+            .fillMaxSize()
+            .border(2.dp, Color.Blue),
+        factory = { mapView }
+    )
 }
